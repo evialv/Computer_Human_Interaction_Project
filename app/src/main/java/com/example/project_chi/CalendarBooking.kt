fa2d4fb4e1f5a93c33316daaea59c255b40d3ad1
evialv <evialvanaki@gmail.com>
2021-06-05 23:14:57 +0300

    adding timeslots

--- app/src/main/java/com/example/project_chi/CalendarBooking.kt
+++ app/src/main/java/com/example/project_chi/CalendarBooking.kt
@@ -1,7 +1,7 @@
 package com.example.project_chi
 import android.os.Bundle
 import android.widget.CalendarView
-import android.widget.Toast
+
 import androidx.appcompat.app.AppCompatActivity
 import com.example.project_chi.R
 //import kotlinx.android.synthetic.main.activity_calendar_booking.*
@@ -12,18 +12,6 @@ class CalendarBooking : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calendar_booking)
-
-        var calendar_view = findViewById<CalendarView>(R.id.calendar_view)
-
-        calendar_view.setOnDateChangeListener(object : CalendarView.OnDateChangeListener {
-            override fun onSelectedDayChange(p0: CalendarView, p1: Int, p2: Int, p3: Int) {
-                Toast.makeText(
-                    this@CalendarBooking,
-                    "The selected date is $p3.${p2 + 1}.$p1",
-                    Toast.LENGTH_SHORT
-                ).show()
-            }
-        })
 
     }
 
